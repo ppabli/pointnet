e1bc549e7f7232edf893766066e05d7253d291c6
Pablo Liste Cancela <ppabli12@gmail.com>
2025-04-24 09:13:25 +0200

    fix: Minor fixes and improvements

--- commands.sh
+++ commands.sh
@@ -1,8 +1,8 @@
 # Train PointNet
-python main.py --action train --model pointnet --data_dir "/media/pablo/Disco programas/datasets/open3d/Kitti" --output_dir ./output --epochs 50 --batch_size 32 --num_points 4096 --feature_transform
+python main.py --action train --model pointnet --data_dir "/media/pablo/Disco programas/datasets/open3d/Kitti" --output_dir ./output --epochs 50 --batch_size 32 --num_points 1024 --feature_transform
 
 # Train PointNet++
-python main.py --action train --model pointnetpp --data_dir "/media/pablo/Disco programas/datasets/open3d/Kitti" --output_dir ./output --epochs 50 --batch_size 32 --num_points 4096
+python main.py --action train --model pointnetpp --data_dir "/media/pablo/Disco programas/datasets/open3d/Kitti" --output_dir ./output --epochs 50 --batch_size 32 --num_points 1024
 
 # Test PointNet
 python main.py --action test --model pointnet --data_dir "/media/pablo/Disco programas/datasets/open3d/Kitti" --output_dir ./output --model_path ./output/pointnet_best.pth
